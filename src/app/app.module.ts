f1c5fb9c8ea6cbf18b84811e481c0eaaec5cfe87
EdwinPrado <xedwin16@gmail.com>
2018-10-24 16:22:24 -0500

    Primer commit del proyecto aprendiendo angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule, Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { PresupuestosComponent } from './presupuestos/presupuestos.component';
+import { CopyrightComponent } from './copyright/copyright.component';
+import { ProveedoresService } from './servicios/proveedores.service';
+import { ProveedoresComponent } from './proveedores/proveedores/proveedores.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { HeaderComponent } from './header/header.component';
+import { AddproveeComponent } from './proveedores/addprovee/addprovee.component';
+
+const routes: Routes=[{ path: '', component: InicioComponent },
+  { path: 'proveedores', component: ProveedoresComponent },
+  { path: 'addprovee', component: AddproveeComponent},
+  { path: '', component: InicioComponent},
+  { path: '**', component: InicioComponent}]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PresupuestosComponent,
+    CopyrightComponent,
+    ProveedoresComponent,
+    InicioComponent,
+    HeaderComponent,
+    AddproveeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    FormsModule
   ],
-  providers: [],
+  providers: [ProveedoresService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
