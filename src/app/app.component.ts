d6c9c1ead4c56c2dd347691d9f891bc3d0e0def8
EdwinPrado <xedwin16@gmail.com>
2018-10-26 17:38:33 -0500

    Se agrego conexion con servidor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Aplicacion-';
+  ngOnInit () {
+    firebase.initializeApp({
+    apiKey: 'AIzaSyA9is2mslut0EWxVAASh3piGmdbcA2Em-A',
+    authDomain: 'comprasapp-83618.firebaseapp.com'
+    });
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule, Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { PresupuestosComponent } from './presupuestos/presupuestos.component';
+import { PresupuestosComponent } from './presupuestos/presupuestos/presupuestos.component';
 import { CopyrightComponent } from './copyright/copyright.component';
 import { ProveedoresService } from './servicios/proveedores.service';
 import { ProveedoresComponent } from './proveedores/proveedores/proveedores.component';
@@ -13,11 +14,19 @@ import { InicioComponent } from './inicio/inicio.component';
 import { HeaderComponent } from './header/header.component';
 import { AddproveeComponent } from './proveedores/addprovee/addprovee.component';
 import { AddpresComponent } from './presupuestos/addpres/addpres.component';
+import { PresupuestosService } from 'src/app/servicios/presupuestos.service';
+import { EditpresComponent } from './presupuestos/editpres/editpres.component';
+import { RegistroComponent } from './autenticacion/registro/registro.component';
+import { AutenticacionService } from './servicios/autenticacion.service';
+
 
 const routes: Routes=[{ path: '', component: InicioComponent },
   { path: 'proveedores', component: ProveedoresComponent },
+  { path: 'editpres/:id', component: EditpresComponent },
   { path: 'addprovee', component: AddproveeComponent},
   { path: 'addpres', component: AddpresComponent},
+  { path: 'presupuestos', component: PresupuestosComponent },
+  { path: 'registro', component: RegistroComponent },
   { path: '', component: InicioComponent},
   { path: '**', component: InicioComponent},
   ]
@@ -31,15 +40,18 @@ const routes: Routes=[{ path: '', component: InicioComponent },
     InicioComponent,
     HeaderComponent,
     AddproveeComponent,
-    AddpresComponent
+    AddpresComponent,
+    EditpresComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes),
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpModule
   ],
-  providers: [ProveedoresService],
+  providers: [ProveedoresService, PresupuestosService, AutenticacionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
